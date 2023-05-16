d0139a099437b7b2db84ba0a1065126a2ddfc9a0
Alex Silva <alex.silva@avioconsulting.com>
2023-05-16 15:13:26 -0300

    aula 7 - streams leitrua

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -11,3 +11,27 @@
 // 10 mb/s -> 10.000 linhas ja poderiam ser inseridas
 
 // Readable Streams / Writable Streams
+
+// Streams ->
+
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+	index = 1;
+	_read() {
+		const i = this.index++;
+
+		setTimeout(() => {
+			if (i > 100) {
+				this.push(null);
+			} else {
+				const buf = Buffer.from(String(i));
+				this.push(buf);
+			}
+		}, 100);
+	}
+}
+
+new OneToHundredStream().pipe(process.stdout);
