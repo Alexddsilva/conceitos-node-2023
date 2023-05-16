7ae499cb9686827c282aae667bf76f230fa356a8
Alex Silva <alex.silva@avioconsulting.com>
2023-05-16 14:38:46 -0300

    aula 3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
 import http from "node:http";
 
+//GET, POST, PUT, PATCH, DELETE
+
 const server = http.createServer((req, res) => {
+	const { method, url } = req;
+
+	if (method === "GET" && url === "/users") {
+		return res.end("Listagem de usuarios");
+	}
+
+	if (method === "POST" && url === "/users") {
+		return res.end("Criação de usuarios");
+	}
+
+	console.log(method, url);
+
 	return res.end("Hello World auto change");
 });
 
