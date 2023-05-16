6c8bad6f5dfed0309d080b1df03cc32be5b2b222
Alex Silva <alex.silva@avioconsulting.com>
2023-05-16 15:02:02 -0300

    aula 6 streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & Spotify
+
+// Importação clientes CSV (Excel)
+// 1Gb - 1.000.000 linhas
+// Post /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Começar as inserções no banco
+
+// 10 mb/s -> 10.000 linhas ja poderiam ser inseridas
+
+// Readable Streams / Writable Streams
