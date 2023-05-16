28e61b4b0acc6206945f28e11b28537ea32dc6ab
Alex Silva <alex.silva@avioconsulting.com>
2023-05-16 15:24:15 -0300

    aula 8 - leitura escrita e transformacao streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -16,7 +16,7 @@
 
 // process.stdin.pipe(process.stdout);
 
-import { Readable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundredStream extends Readable {
 	index = 1;
@@ -34,4 +34,20 @@ class OneToHundredStream extends Readable {
 	}
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class MultiplyByTenStream extends Writable {
+	_write(chunk, encoding, callback) {
+		console.log(Number(chunk.toString()) * 10);
+		callback();
+	}
+}
+
+class InverseNumberStream extends Transform {
+	_transform(chunk, encoding, callback) {
+		const transformed = Number(chunk.toString()) * -1;
+		callback(null, Buffer.from(String(transformed)));
+	}
+}
+
+new OneToHundredStream()
+	.pipe(new InverseNumberStream())
+	.pipe(new MultiplyByTenStream());
