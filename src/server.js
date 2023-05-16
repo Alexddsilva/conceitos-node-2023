d202982b6ed1d8c0e54f2bf0fddbcde3b0056edf
Alex Silva <alex.silva@avioconsulting.com>
2023-05-16 14:47:27 -0300

    aula 4

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 import http from "node:http";
 
 //GET, POST, PUT, PATCH, DELETE
+//JSON - JavaScript Object Notation
+//Cabeçalhos (Request/response) => Metadata
+
+const users = [];
 
 const server = http.createServer((req, res) => {
 	const { method, url } = req;
 
 	if (method === "GET" && url === "/users") {
-		return res.end("Listagem de usuarios");
+		return res
+			.setHeader("Content-type", "application/json")
+			.end(JSON.stringify(users));
 	}
 
 	if (method === "POST" && url === "/users") {
+		users.push({
+			id: 1,
+			name: "John Doe",
+			email: "jhondoe@example.com",
+		});
 		return res.end("Criação de usuarios");
 	}
 
